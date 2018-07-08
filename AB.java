6bb6bf0f59e7728bf7124493fa6e47f86c0fb4bd
rusi <asifec7@gmail.com>
2018-07-08 20:53:44 +0530

    by asif

---
+++ AB.java
@@ -0,0 +1,9 @@
+package i1;
+
+public class AB {
+
+	public static void main(String[] args) {
+		System.out.println("AB class ");
+	}
+
+}
--- cnterview.java
+++ cnterview.java
@@ -1,79 +1,10 @@
 package i1;
 
 public class cnterview {
-//static int space=1;
-//static int word=1;
-//static int j,k=1;
-	static int i,j;
-	static int temp;
-	static int fact=1;
+
 	public static void main(String[] args) {
 		
-//		String str="count the value of asifqq";
-//		for(int i=0;i<str.length()-1;i++)
-//		{
-//			if(str.charAt(i)==' ')
-//			{
-//				space++;
-//			}
-//			else
-//			word++;	
-//		}
-//		System.out.println("number of spaces "+space);
-//		System.out.println("number of word "+word);
-		//-----Descending order
-//		Integer arr[]={10,1,20,12,9,111,8};
-//		Arrays.sort(arr,Collections.reverseOrder());
-//		System.out.println(Arrays.toString(arr));
-//	
-//		//-----Ascending order
-//		Integer arr1[]={12,5,26,33,78};
-//		//Arrays.sort(arr1);
-//		System.out.println(Arrays.toString(arr1));
-//				
-//		
-		//AScending order
-		
-		int arr[]={4,3,2,6,1,5};
-		
-//		 for(i=0;i<arr.length;i++)
-//		 {
-//		 for(j=i+1;j<arr.length;j++)
-//		 {
-//		  if(arr[i]< arr[j])
-//		  {
-//		   temp=arr[i];
-//		   arr[i]=arr[j];
-//		   arr[j]=temp;
-//		  }
-//		 }
-// 		 }
-//
-//		 for(i=0;i<arr.length;i++)
-//		 {
-//		   System.out.println(arr[i]); 
-//		}
-
-		
-//		temp=arr[0];
-//		for(i=1;i<arr.length;i++)
-//		{
-//			if(temp>arr[i])
-//			{
-//				temp=arr[i];
-//			}
-//			
-//		}
-//		System.out.println(temp);
-			
-		//find factorial for given number?
-		
-		for(i=10;i>1;i--)
-		{
-			fact=fact*i;
-		}
-		System.out.println(fact);		
-		
+		System.out.println("cnterview  class ");
 		
 		
 		
