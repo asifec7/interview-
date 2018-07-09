dcb7eee43df13236007e77d1ab167a691a8a02fc
asifec7 <asifec7@gmail.com>
2018-07-09 11:41:02 +0530

    added interview class pull request

--- cnterview.java
+++ cnterview.java
@@ -5,6 +5,7 @@ public class cnterview {
 	public static void main(String[] args) {
 		
 		System.out.println("cnterview  class ");
+		System.out.println("cnterview pull request class ");
 		
 		
 		
